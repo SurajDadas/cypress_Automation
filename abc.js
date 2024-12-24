c355098634b3c6d988ad0a62a8e46d065aca7fd8
Suraj_Dadas <dadas.suraj96@gmail.com>
2024-12-24 21:14:00 +0530

    code updated

--- abc.js
+++ abc.js
@@ -4,8 +4,14 @@ console.log(a)
 //printing the variable having value 'Hello World'
 
 // pull this code to your working directory
+<<<<<<< Updated upstream
 
 
 let C = 10
 let D =20
 console.log(C+D)
+=======
+let A =10
+let B = 20
+console.log(A+B)
+>>>>>>> Stashed changes
